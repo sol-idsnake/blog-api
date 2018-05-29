f26120adb005e41b344dc8cff21b1ae28fd09980
Hendrik W <h.wendt@gmx.com>
2018-05-29 10:10:20 -0400

    initial commit

---
+++ seeds.js
@@ -0,0 +1,25 @@
+[
+{
+	name: 'blogPost-1',
+	title
+	content
+	author-name
+	publication-date
+},
+{
+	name: 'blogPost-2',
+	title
+	content
+	author-name
+	publication-date
+},
+{
+	name: 'blogPost-2',
+	title
+	content
+	author-name
+	publication-date
+}
+]
+
+module.exports = {blogSeeds};
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {BlogPosts} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+const {blogSeeds} = require('./seeds')
+
+// log the http layer
+app.use(morgan('common'));
+

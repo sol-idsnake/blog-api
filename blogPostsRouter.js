0d3c41c90f4e61273ce30a1ba233ebf5acbc2751
Hendrik Wendt <h.wendt@gmx.com>
2018-06-13 19:52:42 -0400

    added PUT, DELETE method

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -23,15 +23,14 @@ router.get('/posts', (req, res) => {
 		})
 })
 
-// 5b1fc5422691867a687f1d82
 router.get('/posts/:id', (req, res) => {
 	BlogPost
 		.findById(req.params.id)
 		.then(post => {
-			console.log(post)
 			const postjson = post.serialize()
 			res.json(postjson)
 		})
+		.catch(err => res.status(500).json({message: 'Internal server error'}))
 })
 
 router.post('/posts', (req,res) => {
@@ -58,52 +57,38 @@ router.post('/posts', (req,res) => {
 		})
 })
 
-// router.post('/', jsonParser, (req, res) => {
-// 	const requiredFields = ['title', 'content', 'author']
-// 	for (let i=0; i<requiredFields.length; i++) {
-// 		const field = requiredFields[i]
-// 		if (!(field in req.body)) {
-// 			const message = `Missing ${field} in request body`
-// 			console.error(message)
-// 			return res.status(400).send(message)
-// 		}
-// 	}
-// 	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author)
-// 	res.status(201).json(item)
-// })
+router.put('/posts/:id', (req, res) => {
+	if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		const message = (
+	      `Request path id (${req.params.id}) and request body id ` +
+	      `(${req.body.id}) must match`);
+	    console.error(message);
+	    return res.status(400).json({ message: message });
+	}
 
-// router.put('/:id', jsonParser, (req, res) => {
-// 	const requiredFields = ['title', 'content', 'author', 'publishDate'];
-// 	for (let i=0; i<requiredFields.length; i++) {
-// 		const field = requiredFields[i];
-// 		if (!(field in req.body)) {
-// 			const message = `Missing \`${field}\` in request body`
-// 			console.error(message);
-// 			return res.status(400).send(message);
-// 		}
-// 	}
-// 	if (req.params.id !== req.body.id) {
-// 	    const message = (
-// 			`Request path id (${req.params.id}) and request body id `
-// 			`(${req.body.id}) must match`);
-// 			console.error(message);
-// 		return res.status(400).send(message);
-// 	}
-// 	console.log(`Updating Blog Post with ID \`${req.params.id}\``);
-// 	const updatedItem = BlogPosts.update({
-// 		id: req.params.id,
-// 		title: req.body.title,
-// 		content: req.body.content,
-// 		author: req.body.author,
-// 		publishDate: req.body.publishDate
-// 	});
-// 	res.status(204).end();
-// })
+	const toUpdate = {}
+	const updateableFields = ['title', 'content', 'author']
 
-// router.delete('/:id', (req, res) => {
-// 	BlogPosts.delete(req.params.id)
-// 	console.log(`Deleted blog post with ID ${req.params.id}`)
-// 	res.status(204).end()
-// })
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			toUpdate[field] = req.body[field]
+		}
+	})
+
+	BlogPost
+		.findByIdAndUpdate(req.params.id, {$set: toUpdate}, { new: true })
+		.then(post => res.status(204).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}))
+})
+
+router.delete('/posts/:id', (req, res) => {
+	BlogPost
+		.findByIdAndRemove(req.params.id)
+		.then(() => {
+			console.log(`Deleted post with ID \`${req.params.id}\``)
+			res.status(204).end()
+		})
+		.catch(err => res.status(500).json({message: 'Internal server error'}))
+})
 
 module.exports = router
